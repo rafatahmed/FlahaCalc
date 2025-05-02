4c4f15f179f2f52d51bb8e328aefb8ab31e0086b
Rafat A. Al Khashan <eng.rafatahmed@hotmail.com>
2025-05-02 20:07:04 +0300

    deployment

---
+++ scripts/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+echo "Starting build process..."
+
+# Check if webpack is installed
+if [ -f "./node_modules/.bin/webpack" ]; then
+  echo "Webpack found in node_modules"
+else
+  echo "Webpack not found in node_modules, installing..."
+  npm install --save-dev webpack webpack-cli
+fi
+
+# Run webpack with verbose output
+echo "Running webpack..."
+./node_modules/.bin/webpack --config webpack.config.js --display-error-details
+
+echo "Build completed"
